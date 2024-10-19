bc682f3bd4de5fe8883d84dc0ff5f321eb990bad
Paulo Akira <paulo@enabe.com.br>
2024-10-19 16:46:57 -0300

    Create a VM in GCP

---
+++ gcp_setup_vm.sh
@@ -0,0 +1,22 @@
+# VM information (modify as necessary)
+VM_NAME=cpu-vm
+ZONE=us-central1-a
+
+# Create a VM instance
+gcloud compute instances create $VM_NAME \
+  --zone=$ZONE \
+  --machine-type=n1-standard-4 \
+  --image-family=ubuntu-2004-lts \
+  --image-project=ubuntu-os-cloud \
+  --boot-disk-size=100GB \
+  --boot-disk-type=pd-standard \
+  --tags=http-server,https-server
+
+# Assing the role to access Artifact Registry
+gcloud projects add-iam-policy-binding golden-torch-439114-e4 \
+  --member="serviceAccount:894352243947-compute@developer.gserviceaccount.com" \
+  --role="roles/artifactregistry.reader"
+
+# Authenticate in GCP
+gcloud auth login
+gcloud auth configure-docker
