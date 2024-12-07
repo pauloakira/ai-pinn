d993d92593d4c3aed307e830774c84fd8902e959
Paulo Akira <paulo@enabe.com.br>
2024-12-07 18:32:46 -0300

    Inference pipeline

--- inference.py
+++ inference.py
@@ -4,10 +4,31 @@ import numpy as np
 
 import core.neural_backend as neural
 
-def load_model(model):
-    pass
+def load_model(model_path: str, ndof: int, input_dim_nodes: int, input_dim_materials: int):
+    nodes_layers = [128, 256, 512, 512, 512, 512]  # Layers for nodes sub-network
+    material_layers = [128, 128, 256, 256, 512, 512, 512, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 1024, 2048, 2048, 4096, 4096, 2048, 2048, 1024, 1024, 1024, 1024, 512, 512] # Layers for materials sub-network
+    final_layers = [1024, 1024, 1024, 1024, 1024, 1024] # Layers for final combination network
 
-def run_test(model, consolidated_file_path: str, geometry_file_path: str):
+    # Create a new instance of the model (with the same architecture)
+    loaded_model = neural.BeamApproximatorWithMaterials(
+        input_dim_nodes=input_dim_nodes, 
+        input_dim_materials=input_dim_materials, 
+        nodes_layers=nodes_layers, 
+        material_layers=material_layers, 
+        final_layers=final_layers, 
+        ndof=ndof
+    )
+
+    # Load the saved model state
+    loaded_model.load_state_dict(torch.load(model_path))
+
+    # Set the model to evaluation mode (important for inference)
+    loaded_model.eval()
+
+    return loaded_model
+
+
+def run_test(model_path: str, consolidated_file_path: str, geometry_file_path: str, type: str = "beam"):
     # Load test dataset
     with open(consolidated_file_path, 'r') as file:
         dataset = json.load(file)
@@ -21,6 +42,18 @@ def run_test(model, consolidated_file_path: str, geometry_file_path: str):
     nodes = np.array(geometry['nodes'])
     nodes = nodes.flatten()
     nodes = torch.tensor(nodes, dtype=torch.float32, requires_grad=True)
+
+    # Number of degrees of freedom
+    if type == "beam":
+        ndof = 2*len(nodes)
+        input_dim_nodes = 2*len(nodes)
+        input_dim_materials = 3
+    elif type == "portic":
+        ndof = 3*len(nodes)
+        input_dim_nodes = 2*len(nodes)
+        input_dim_materials = 3
+    else:
+        raise("Invalid type.")
 
     for data in loaded_dataset:
         # Consolidate material parameters
@@ -36,6 +69,11 @@ def run_test(model, consolidated_file_path: str, geometry_file_path: str):
         # Normalizing
         nodes, material_params = neural.normalize_inputs(nodes, material_params)
 
+        model = load_model(model_path, 
+                        ndof=ndof, 
+                        input_dim_nodes=input_dim_nodes, 
+                        input_dim_materials=input_dim_materials)
+
         predicted_displacements, l2_error, energy_error, h1_error, inference_time = neural.test_portic(
             nodes=nodes,
             material_params=material_params,
@@ -45,12 +83,16 @@ def run_test(model, consolidated_file_path: str, geometry_file_path: str):
             verbose=True
         )
 
+        print(h1_error)
+        break
 
 if __name__ == "__main__":
     # Paths
     file_path = "data/consolidated_beam_test_data.json"
     geometry_path = "data/geometries/beam_64.json"
+    model_path = "data/models/neural_vem_beam_64.pth"
 
     # Run test pipeline
-    run_test(consolidated_file_path=file_path,
-            geometry_file_path=geometry_path)
+    run_test(model_path=model_path,
+        consolidated_file_path=file_path,
+        geometry_file_path=geometry_path)
